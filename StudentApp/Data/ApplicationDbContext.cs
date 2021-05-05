e3f3d52e349da6e7cf3fc64b403ba156aabf0c87
Özge Asar <ozasgear@gmail.com>
2021-05-06 00:46:45 +0300

    Add project files.

---
+++ StudentApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudentApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ StudentApp/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int PageCount { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
